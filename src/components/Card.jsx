33223a637702b0e90c04225e8f030bf41c25e7ab
epinedo <epinedo@robotmonkeyllc.com>
2024-07-23 23:01:08 -0400

    added detail labels by category

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,43 @@
 import { useContext } from "react";
 import SWContext from "../SWContext.jsx";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Card({ data }) {
     const { taskActions } = useContext(SWContext);
-
-    const navigate = useNavigate();
 
     function handleFavorite(name) {
         taskActions({ type: "toggleFav", payload: name });
     }
     
+    const detailLabels = {
+        people: {
+            name: ['gender', 'hair_color', 'eye_color'],
+            label: ['Gender:', 'Hair Color:', 'Eye Color:']
+        },
+        planets: {
+            name: ['terrain', 'population', 'climate'],
+            label: ['Terrain:', 'Population:', 'Climate:']
+        },
+        vehicles: {
+            name: ['model', 'manufacturer', 'cost_in_credits'],
+            label: ['Model:', 'Manufacturer:', 'Cost in Credits:']
+        }
+    }
+
     return (
         <div className="card col-5">
             <img className="card-img-top" src="..." alt="Card image cap"/>
             <div className="card-body">
                 <h5 className="card-title">{data.name}</h5>
-                <p className="card-text">Gender: {data.gender}</p>
+                {/* <p className="card-text">Gender: {data.gender}</p>
                 <p className="card-text">Hair Color: {data.hair_color}</p>
-                <p className="card-text">Eye Color: {data.eye_color}</p>
+                <p className="card-text">Eye Color: {data.eye_color}</p> */
+                detailLabels[data.category].label.map((x,i) => (
+                    <p key={i} className="card-text">
+                        {x} {data[detailLabels[data.category].name[i]]}
+                    </p>
+                ))
+                }
                 <div>
                     <Link to="/details/1">Learn More</Link>
                     <button onClick={() => handleFavorite(data.name)} className={"btn " +( data.isFavorite ? 'active' : '')}>Favorite</button>
