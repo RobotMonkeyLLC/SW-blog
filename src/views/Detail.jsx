b4a78686a9486de6e38d6289e10b39d2429a9e9c
epinedo <epinedo@robotmonkeyllc.com>
2024-07-25 00:30:24 -0400

    attempting pipe functions

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -4,22 +4,47 @@ import { Link, useParams } from "react-router-dom";
 import detailLabels from "../components/detailLabels.jsx";
 
 export default function Detail() {
-    let { id, category } = useParams();
+    const { id, category } = useParams();
     const { tasks, taskActions } = useContext(SWContext);
+
+    /* const taskSubset =  tasks.find(x => x.category == category.replace('people', 'characters').toLowerCase() && x.path == (category + '/' + id + '/'));
+    const detailSubset = detailLabels[category.replace('people', 'characters')].detail.name;
+    const detailContent = detailSubset.map(x=>taskSubset[x]); */
+
+    const taskSubset = (tasks) => {
+        return tasks.find(x => x.category == category.replace('people', 'characters').toLowerCase() && x.path == (category + '/' + id + '/'));
+    }
+    const detailSubset = detailLabels[category.replace('people', 'characters')].detail.name;
+
+    const detailContent = (tasks) => {
+        return detailSubset.map(x=>tasks[x]);
+    }
+
+    const _reduced = (f, g) => (args) => f(g(args));
+    const pipe = (...fns) => fns.reduce(_reduced);
+
     return (
         <div className="container">
         <div className="row">
             <div className="col-12">
                 <h1>Detail {id} {category}</h1>
-                {
-                    ['','',''].map(
-                        (x,i) => (
-                            <p key={i}>
-                                {i}
-                            </p>
-                        )
-                    )
-                }
+                <div className="row">
+                    {
+                        detailLabels[category.replace('people', 'characters')].detail.label.map((x,i) => (
+                            <div key={i} className="card-text col-3">
+                                <h5>{x}</h5>
+                                <p>
+                                    {
+                                        // too complicated, find a way to simplify this
+                                     
+                                    }
+                                </p>
+                            </div>
+                            
+                        ))
+
+                    }
+                </div>
             </div>
         </div>
         </div>
